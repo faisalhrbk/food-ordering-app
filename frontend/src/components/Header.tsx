968a33d0f8dd13226e11736614d69d142e31e359
faisalhrbk <faisalhrbk@outlook.com>
2025-05-13 23:20:18 +0500

    im really enjoy it an ill know about shadcn

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -6,7 +6,7 @@ export const Header = () => {
 			<div className="container mx-auto flex justify-between items-center">
 				<Link
 					to="/"
-					className="text-3xl font-bold tracking-tight text-orange-500"
+					className="text-3xl font-bold tracking-tight text-orange-500 cursor-pointer"
 				>
 					fakefood.com
 				</Link>
--- frontend/src/layouts/Layout.tsx
+++ frontend/src/layouts/Layout.tsx
@@ -9,7 +9,7 @@ const Layout  = ({children}: Props) => {
 return (
     <div className="flex flex-col min-h-screen">
         <Header />
-        <div className="container mx-auto flex-1 py10">{children}</div>
+        <div className="container mx-auto flex-1 py-10">{children}</div>
     </div>
 )
 }
