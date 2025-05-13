078efbf41cd77bbf8bf84a0b7cce44cb5ce7125d
faisalhrbk <faisalhrbk@outlook.com>
2025-05-13 23:07:51 +0500

    thisis crazy bruhh TS IS overwhelming

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,10 @@
 import { Routes, Route, Navigate } from "react-router-dom";
+import Layout from "./layouts/Layout";
+
 
 const AppRoutes = () => (
 	<Routes>
-		<Route path="/" element={<span>home</span>} />
+		<Route path="/" element={<Layout>CHildren</Layout>} />
 		<Route path="/user-profile" element={<span>profile</span>} />
 		<Route path="/*" element={<Navigate to="/" />} />
 	</Routes>
