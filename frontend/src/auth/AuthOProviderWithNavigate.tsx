83cfd45c67189048f1543c0b706c8de4dc2e28e9
faisalhrbk <faisalhrbk@outlook.com>
2025-05-14 02:09:42 +0500

    AuthOProviderWithNavigate , what the heck is this

---
+++ frontend/src/auth/AuthOProviderWithNavigate.tsx
@@ -0,0 +1,29 @@
+import { type AppState, Auth0Provider, User } from "@auth0/auth0-react";
+type Props = {
+	children: React.ReactNode;
+};
+
+const AuthOProviderWithNavigate = ({ children }: Props) => {
+	const domain = import.meta.env.VITE_AUTH0_DOMAIN;
+	const clientId = import.meta.env.VITE_AUTH0_CLIENT_ID;
+	const redirectUri = import.meta.env.VITE_AUTH0_CALLBACK_URI;
+	if (!domain || !clientId || !redirectUri)
+		throw new Error("unable to initialize auth");
+	const onRedirectCallback = (AppState?: AppState, user?: User) => {
+        console.log(user)
+    };
+	return (
+		<Auth0Provider
+			domain={domain}
+			clientId={clientId}
+			authorizationParams={{
+				redirect_uri: redirectUri,
+			}}
+			onRedirectCallback={onRedirectCallback}
+		>
+			{children}
+		</Auth0Provider>
+	);
+};
+
+export default AuthOProviderWithNavigate;
