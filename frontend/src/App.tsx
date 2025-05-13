983752934475fdadb532bf56a15fbb2c2ed82f99
faisalhrbk <faisalhrbk@outlook.com>
2025-05-14 00:13:41 +0500

    ADDED THE FOOTER

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,17 @@
 import { Routes, Route, Navigate } from "react-router-dom";
 import Layout from "./layouts/Layout";
-
+import HomePage from "./pages/HomePage";
 
 const AppRoutes = () => (
 	<Routes>
-		<Route path="/" element={<Layout>CHildren</Layout>} />
+		<Route
+			path="/"
+			element={
+				<Layout>
+					<HomePage />
+				</Layout>
+			}
+		/>
 		<Route path="/user-profile" element={<span>profile</span>} />
 		<Route path="/*" element={<Navigate to="/" />} />
 	</Routes>
--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,11 +1,29 @@
+import landingImage from "../assets/landing.png";
+import appDownloadIMage from "../assets/appDownload.png";
 const HomePage = () => {
 	return (
 		<div className="flex flex-col gap-12">
-			<div className=" bg-white rounded-lg  flex flex-col gap-5 text-center shadow-md py-8 -mt-16">
+			<div className=" bg-amber-50 rounded-lg  flex flex-col gap-5 text-center shadow-md py-8 -mt-16">
 				<h1 className="text-5xl font-bold tracking-tight text-orange-600">
-					TUck into a takway today
+					Tuck into a takway today
 				</h1>
 				<span className="text-xl "> Food is just click away</span>
+			</div>
+            <div className="grid md:grid-cols-2 gap-5">
+				<div>
+					<img src={landingImage} alt="" />
+				</div>
+
+				<div className="flex flex-col items-center justify-center gap-4 text-center">
+					<span className="font-bold text-3xl tracking-tighter">
+						Order takeway even faster
+					</span>
+					<span>
+						download the OrangePanda App for faster ordering and personalized
+						recommendations
+					</span>
+					<img src={appDownloadIMage} alt="app download img" />
+				</div>
 			</div>
 		</div>
 	);
