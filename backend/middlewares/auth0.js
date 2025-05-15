b34c89e415c23143548cbe14d48c1b6c63ee9eeb
faisalhrbk <faisalhrbk@outlook.com>
2025-05-15 14:42:54 +0500

    i don;t a single line of react

--- backend/middlewares/auth0.js
+++ backend/middlewares/auth0.js
@@ -1,7 +1,8 @@
 import { auth } from "express-oauth2-jwt-bearer";
 
-const jwtCheck = auth({
+ export const jwtCheck = auth({
 	audience: process.env.AUTH0_AUDIENCE,
 	issuerBaseURL: process.env.AUTH0_ISSUER_BASE_URL,
 	tokenSigningAlg: "RS256",
 });
+
--- backend/routes/userRouter.js
+++ backend/routes/userRouter.js
@@ -1,10 +1,9 @@
 import express from "express";
 const userRouter = express.Router();
 import { createCurrentUser } from "../controllers/userController.js";
-
+import { jwtCheck } from "../middlewares/auth0.js";
 
 //* api/user
-userRouter.post("/", createCurrentUser)
-
+userRouter.post("/", jwtCheck, createCurrentUser);
 
 export default userRouter;
