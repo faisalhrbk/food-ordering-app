2fa1e7b01ba4d6fd08e0f3ee8cd8b690040d2771
faisalhrbk <faisalhrbk@outlook.com>
2025-05-15 12:37:48 +0500

    let make a big difference today

---
+++ frontend/src/api/UserApi.tsx
@@ -0,0 +1,19 @@
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+
+type createUserRequest = {
+    auth0Id : string;
+    email: string;
+}
+export const useCreateMyUser = () =>{
+ const createMyUserRequest = async (user: createUserRequest) => {
+    const response = await fetch(`${API_BASE_URL},api/user`, {
+        method: 'POST',
+        headers : {
+            "Content-Type": "application/json"
+        }, body:  JSON.stringify(user)
+    })
+    if(!response.ok){
+        throw new  Error('Failed to create user!')
+    }
+ };   
+}
