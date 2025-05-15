98d35aa35acf5080f16c5a2df0b431fa7d1e9ab6
faisalhrbk <faisalhrbk@outlook.com>
2025-05-15 16:58:08 +0500

    almost created form using zod typescript is love bruhhh

--- frontend/src/forms/user-profile-form/UserProfileForm.tsx
+++ frontend/src/forms/user-profile-form/UserProfileForm.tsx
@@ -1,7 +1,15 @@
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { z } from "zod";
-import { Form } from "@/components/ui/form";
+import {
+	Form,
+	FormControl,
+	FormDescription,
+	FormField,
+	FormItem,
+	FormLabel,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
 const formSchema = z.object({
 	email: z.string().optional(),
 	name: z.string().min(1, "name is required"),
@@ -27,11 +35,75 @@ const UserProfileForm = ({ onSave, isLoading }: UserProfileFormProps) => {
 				onSubmit={form.handleSubmit(onSave)}
 				className="space-y-4 bg-gray-50 rounded-lg md:p-10"
 			>
-<div>
-    <h2 className="text-2xl font-bold">User Profile Form</h2>
-</div>
-
-            </form>
+				<div>
+					<h2 className="text-2xl font-bold">User Profile Form</h2>
+					<FormDescription>
+						view and change your profile information
+						<FormField
+							control={form.control}
+							name="email"
+							render={({ field }) => (
+								<FormItem>
+									<FormLabel>Email</FormLabel>
+									<FormControl>
+										<Input {...field} disabled className="bg-white" />
+									</FormControl>
+								</FormItem>
+							)}
+						/>
+						<FormField
+							control={form.control}
+							name="name"
+							render={({ field }) => (
+								<FormItem>
+									<FormLabel>Name</FormLabel>
+									<FormControl>
+										<Input {...field} disabled className="bg-white" />
+									</FormControl>
+								</FormItem>
+							)}
+						/>
+						<div className="flex flex-col md:flex-row gap-4">
+							<FormField
+								control={form.control}
+								name="addressLine1"
+								render={({ field }) => (
+									<FormItem>
+										<FormLabel>Address Line 1</FormLabel>
+										<FormControl>
+											<Input {...field} disabled className="bg-white" />
+										</FormControl>
+									</FormItem>
+								)}
+							/>
+							<FormField
+								control={form.control}
+								name="city"
+								render={({ field }) => (
+									<FormItem>
+										<FormLabel>city</FormLabel>
+										<FormControl>
+											<Input {...field} disabled className="bg-white" />
+										</FormControl>
+									</FormItem>
+								)}
+							/>
+							<FormField
+								control={form.control}
+								name="country"
+								render={({ field }) => (
+									<FormItem>
+										<FormLabel>country</FormLabel>
+										<FormControl>
+											<Input {...field} disabled className="bg-white" />
+										</FormControl>
+									</FormItem>
+								)}
+							/>
+						</div>
+					</FormDescription>
+				</div>
+			</form>
 		</Form>
 	);
 };
