dddf0f1dce91464739dfbb91f1fd2784000496aa
faisalhrbk <faisalhrbk@outlook.com>
2025-05-15 14:47:56 +0500

    broooo kuch smjh nahi arraha

--- frontend/src/auth/Auth0ProviderWithNavigate.tsx
+++ frontend/src/auth/Auth0ProviderWithNavigate.tsx
@@ -1,4 +1,3 @@
-
 import { Auth0Provider } from "@auth0/auth0-react";
 import { useNavigate } from "react-router-dom";
 type Props = {
@@ -6,18 +5,17 @@ type Props = {
 };
 
 const AuthOProviderWithNavigate = ({ children }: Props) => {
-	const navigate = useNavigate()
-	
+	const navigate = useNavigate();
+
 	const domain = import.meta.env.VITE_AUTH0_DOMAIN;
 	const clientId = import.meta.env.VITE_AUTH0_CLIENT_ID;
 	const redirect_uri = import.meta.env.VITE_AUTH0_CALLBACK_URL;
-	if (!domain || !clientId || !redirect_uri)
+	const audience = import.meta.env.VITE_AUTH0_AUDIENCE;
+	if (!domain || !clientId || !redirect_uri || !audience)
 		throw new Error("unable to initialize auth");
 
 	const onRedirectCallback = () => {
-	
 		navigate("/auth-callback");
-		
 	};
 
 	return (
@@ -26,6 +24,7 @@ const AuthOProviderWithNavigate = ({ children }: Props) => {
 			clientId={clientId}
 			authorizationParams={{
 				redirect_uri: redirect_uri,
+				audience
 			}}
 			onRedirectCallback={onRedirectCallback}
 		>
